334a27fee6a673cea347bafca1d7f8e2f7e052d3
william.quach <william.quach@outlook.fr>
2023-05-11 10:55:11 +0200

    feat(OAuth): Make OAuth work (Google & GitHub)
    Reformat files
    Update prisma versions
    Use indent to 4 spaces

--- next.config.js
+++ next.config.js
@@ -1,22 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  swcMinify: true,
-  images: {
-    domains: ["lh3.googleusercontent.com", "vercel.com"],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/steven-tey/precedent",
-        permanent: false,
-      },
-    ];
-  },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    swcMinify: true,
+    images: {
+        domains: ["lh3.googleusercontent.com", "vercel.com"],
+    },
+    api: {
+        bodyParser: false,
+    },
+    async redirects() {
+        return [
+            {
+                source: "/github",
+                destination: "https://github.com/steven-tey/precedent",
+                permanent: false,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
