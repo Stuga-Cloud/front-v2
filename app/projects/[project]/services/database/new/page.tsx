99a44698e85a61774098c0ca01a63d9ed93b8c18
Swann HERRERA <swann.herrera@gmail.com>
2023-06-03 00:48:17 +0200

    feat(breadcrumb): Fix breadcrumb

--- app/projects/[project]/services/database/new/page.tsx
+++ app/projects/[project]/services/database/new/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { DatabasePage } from "@/components/project/database";
-import Breadcrumb from "@/components/shared/breadcrumb";
+import { Breadcrumb, BreadcrumbItem } from "@/components/shared/breadcrumb";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 const queryClient = new QueryClient();
 
-export default async function NewDatabaasePage() {
+
+export default async function NewDatabasePage() {
+  const { project } = useParams();
+  const breadcrumbItem: BreadcrumbItem[] = [ 
+      {text: "project", slug: `/projects/${project}`},
+      {text: "database", slug: `project", slug: "/projects/${project}/services/database/`},
+    ];
     return (
         <QueryClientProvider client={queryClient}>
             <div className="z-10 flex w-full flex-col items-center justify-center">
                 <div className="mt-10 flex w-4/5 flex-row items-center justify-between">
-                    <Breadcrumb items={["project", "database"]} />
+                    <Breadcrumb items={breadcrumbItem} />
                     <DatabasePage />
                 </div>
             </div>
--- components/shared/breadcrumb.tsx
+++ components/shared/breadcrumb.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Breadcrumb = ({ items }: { items: string[] }) => {
+export interface BreadcrumbItem {
+  text: string,
+  slug: string,
+}
+
+function createUrl(slug: string) {
+return location.protocol + '//' + location.host + slug;
+}
+
+export const Breadcrumb = ({ items }: { items: BreadcrumbItem[] }) => {
+  const localUrl = location.protocol + '//' + location.host;
   return (
-    <nav className="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
+    <nav className="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-3">
         <li className="inline-flex items-center">
-          <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+          <a href={localUrl} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600">
             <svg aria-hidden="true" className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
             </svg>
@@ -13,13 +23,15 @@ const Breadcrumb = ({ items }: { items: string[] }) => {
           </a>
         </li>
 
-    {items.map((item) => (
-        <li aria-current="page">
+    {items.map((item, index) => (
+        <li aria-current="page" key={index}>
           <div className="flex items-center">
-            <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
-            </svg>
-            <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">{item}</span>
+            <a href={createUrl(item.slug)} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600">
+              <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+              </svg>
+              <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2">{item.text}</span>
+            </a>
           </div>
         </li>
     ))}
@@ -28,4 +40,3 @@ const Breadcrumb = ({ items }: { items: string[] }) => {
   );
 };
 
-export default Breadcrumb;
