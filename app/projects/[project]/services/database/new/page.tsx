c557e5ce323bb666535fdfa2861b4ea5dc07b6c0
Swann HERRERA <swann.herrera@gmail.com>
2023-06-02 19:17:47 +0200

    feat(breadcrumb): create and add breadcrumb to the app

--- app/projects/[project]/services/database/new/page.tsx
+++ app/projects/[project]/services/database/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { DatabasePage } from "@/components/project/database";
+import Breadcrumb from "@/components/shared/breadcrumb";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -9,6 +10,7 @@ export default async function NewDatabaasePage() {
         <QueryClientProvider client={queryClient}>
             <div className="z-10 flex w-full flex-col items-center justify-center">
                 <div className="mt-10 flex w-4/5 flex-row items-center justify-between">
+                    <Breadcrumb items={["project", "database"]} />
                     <DatabasePage />
                 </div>
             </div>
