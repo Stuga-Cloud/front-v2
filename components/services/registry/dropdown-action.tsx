ffffccfda2d1b24b3d46acb7d372480ce455febb
Rémy Machavoine <remygt@hotmail.fr>
2023-06-04 23:21:01 +0200

    resolve dropdown close problem

--- components/services/registry/dropdown-action.tsx
+++ components/services/registry/dropdown-action.tsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const DropdownAction = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const ref = useRef(null);
 
     const handleButtonClick = () => {
         setIsOpen(!isOpen);
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className="relative inline-block text-left">
+        <div className="relative inline-block text-left" ref={ref}>
             <div>
                 <button
                     type="button"
@@ -19,18 +34,6 @@ export const DropdownAction = () => {
                     onClick={handleButtonClick}
                 >
                     Actions
-                    {/* <svg
-                        className="-mr-1 h-5 w-5 text-gray-400"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                            clipRule="evenodd"
-                        />
-                    </svg> */}
                 </button>
             </div>{" "}
             {isOpen && (
@@ -53,37 +56,6 @@ export const DropdownAction = () => {
                         </a>
                     </div>
                 </div>
-                // <a
-                //     href="#"
-                //     className="block px-4 py-2 text-sm text-gray-700"
-                //     role="menuitem"
-                //     tabIndex={-1}
-                //     id="menu-item-1"
-                // >
-                //     Support
-                // </a>
-                // <a
-                //     href="#"
-                //     className="block px-4 py-2 text-sm text-gray-700"
-                //     role="menuitem"
-                //     tabIndex={-1}
-                //     id="menu-item-2"
-                // >
-                //     License
-                // </a>
-                // <div className="py-1" role="none">
-                //     <form method="POST" action="#" role="none">
-                //         <button
-                //             type="submit"
-                //             className="block w-full px-4 py-2 text-left text-sm text-gray-700"
-                //             role="menuitem"
-                //             tabIndex={-1}
-                //             id="menu-item-3"
-                //         >
-                //             Sign out
-                //         </button>
-                //     </form>
-                // </div>
             )}
         </div>
     );
