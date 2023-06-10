ee1ad52e5baa2b14d22a85f2dd96fbe9fa26bfe0
Rémy Machavoine <remygt@hotmail.fr>
2023-06-10 10:15:34 +0200

    fix(duplicate): add a resolution for duplicate errors

--- lib/next-response.ts
+++ lib/next-response.ts
@@ -1,42 +1,5 @@
 import { NextResponse } from "next/server";
 
-class ResponseService {
-    static success(json: Record<string, any>, init?: ResponseInit) {
-        return NextResponse.json(json, init);
-    }
-
-    static created(json: Record<string, any>, init?: ResponseInit) {
-        return NextResponse.json(json, { ...init, status: 201 });
-    }
-
-    static unauthorized(message = "Unauthorized", init?: ResponseInit) {
-        return NextResponse.json({ error: message }, { ...init, status: 401 });
-    }
-
-    static notFound(message = "Not found", init?: ResponseInit) {
-        return NextResponse.json({ error: message }, { ...init, status: 404 });
-    }
-
-    static methodNotAllowed(
-        message = "Method Not Allowed",
-        init?: ResponseInit,
-    ) {
-        return NextResponse.json({ error: message }, { ...init, status: 405 });
-    }
-
-    static internalServerError(
-        message = "Internal Server Error",
-        init?: ResponseInit,
-    ) {
-        return NextResponse.json({ error: message }, { ...init, status: 500 });
-    }
-
-    static conflict(message = "Conflict", init?: ResponseInit) {
-        return NextResponse.json({ error: message }, { ...init, status: 409 });
-    }
-}
-
-=======
 interface ErrorBody {
     error: string;
     context?: string;
@@ -53,13 +16,13 @@ class ResponseService {
 
     static unauthorized(
         message = "Unauthorized",
-        errorContext?: Error,
+        errorContext?: any,
         init?: ResponseInit,
     ) {
         let errorBody: ErrorBody = { error: message };
-        if (process.env.NODE_ENV === "development") {
+        if (this.hasToDisplayErrorContext(errorContext)) {
             errorBody = {
-                context: errorContext?.stack ?? "no context",
+                context: errorContext.stack,
                 ...errorBody,
             };
         }
@@ -68,13 +31,13 @@ class ResponseService {
 
     static notFound(
         message = "Not found",
-        errorContext?: Error,
+        errorContext?: any,
         init?: ResponseInit,
     ) {
         let errorBody: ErrorBody = { error: message };
-        if (process.env.NODE_ENV === "development") {
+        if (this.hasToDisplayErrorContext(errorContext)) {
             errorBody = {
-                context: errorContext?.stack ?? "no context",
+                context: errorContext.stack,
                 ...errorBody,
             };
         }
@@ -83,17 +46,33 @@ class ResponseService {
 
     static methodNotAllowed(
         message = "Method Not Allowed",
-        errorContext?: Error,
+        errorContext?: any,
         init?: ResponseInit,
     ) {
         let errorBody: ErrorBody = { error: message };
-        if (process.env.NODE_ENV === "development") {
+        if (this.hasToDisplayErrorContext(errorContext)) {
             errorBody = {
-                context: errorContext?.stack ?? "no context",
+                context: errorContext.stack,
                 ...errorBody,
             };
         }
         return NextResponse.json(errorBody, { ...init, status: 405 });
+    }
+
+    static conflict(
+        message = "Conflict",
+        errorContext?: any,
+        init?: ResponseInit,
+    ) {
+        let errorBody: ErrorBody = { error: message };
+        if (this.hasToDisplayErrorContext(errorContext)) {
+            errorBody = {
+                context: errorContext.stack,
+                ...errorBody,
+            };
+        }
+
+        return NextResponse.json({ error: message }, { ...init, status: 409 });
     }
 
     static internalServerError(
@@ -102,15 +81,20 @@ class ResponseService {
         init?: ResponseInit,
     ) {
         let errorBody: ErrorBody = { error: message };
-        if (process.env.NODE_ENV === "development") {
-            if (errorContext instanceof Error) {
+        if (this.hasToDisplayErrorContext(errorContext)) {
                 errorBody = {
                     context: errorContext.stack,
                     ...errorBody,
                 };
-            }
         }
         return NextResponse.json(errorBody, { ...init, status: 500 });
+    }
+
+    private static hasToDisplayErrorContext(errorContext?: any) {
+        return (
+            process.env.NODE_ENV === "development" &&
+            errorContext instanceof Error
+        );
     }
 }
 
